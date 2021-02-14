1b6e788dcf53551f126103c862880c19b02d5bd5
Roman Pronskiy <roman@pronskiy.com>
2021-02-14 12:24:09 +0200

    Init

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+class RandPhpText
+{
+    private static ?array $texts = null;
+
+    public static function getRandomLine()
+    {
+        self::ensureTextsLoaded();
+
+        $probabilityArray = [];
+
+        foreach (self::$texts as $key => $tweet) {
+            $lengthFilledProbability = array_fill(0, $tweet['probability'], $key);
+            $probabilityArray = array_merge($probabilityArray, $lengthFilledProbability);
+        }
+
+        $length = count($probabilityArray);
+        $randomKey = random_int(0, $length-1);
+        $tweetKey = $probabilityArray[$randomKey];
+
+
+//        var_dump(self::$texts[$tweetKey]);
+        return self::$texts[$tweetKey]['text'];
+    }
+
+    private static function ensureTextsLoaded()
+    {
+        if (null === self::$texts) {
+            self::$texts = include __DIR__ . '/tweets.php';
+        }
+    }
+}
+
+$CONSUMER_KEY = getenv('CONSUMER_KEY');
+$CONSUMER_SECRET = getenv('CONSUMER_SECRET');
+$ACCESS_TOKEN = getenv('ACCESS_TOKEN');
+$ACCESS_TOKEN_SECRET = getenv('ACCESS_TOKEN_SECRET');
+
+$connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+
+return function ($event) use ($connection)
+{
+    $result = [];
+    $randomLine = RandPhpText::getRandomLine();
+    $result[] = $randomLine;
+
+    $statues = $connection->post("statuses/update", ["status" => $randomLine]);
+
+    $result[] = $statues;
+    return json_encode($result, JSON_PRETTY_PRINT);
+};
---
+++ tweets.php
@@ -0,0 +1,121 @@
+<?php
+
+class DyingPhp
+{
+    public const TYPE_YES = 1;
+    public const TYPE_NO = 0;
+}
+
+return [
+    [
+        'text' => 'Yes',
+        'probability' => 0,
+        'type' => DyingPhp::TYPE_YES,
+    ],
+    [
+        'text' => 'No',
+        'probability' => 50,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'I don’t know! I’m just a bot! What do you want from me? ',
+        'probability' => 5,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Who cares',
+        'probability' => 5,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Oh come on',
+        'probability' => 5,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'If you think about it: we’re all dying.',
+        'probability' => 5,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'You wish!',
+        'probability' => 10,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Dunno',
+        'probability' => 2,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Try again later.',
+        'probability' => 2,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'rand(0, 1) ? \'Yes\' : \'No\'',
+        'probability' => 5,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Don’t count on it.',
+        'probability' => 5,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Whatever',
+        'probability' => 5,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'You can’t be dying if you’re already dead. I’m talking to you Ruby.',
+        'probability' => 1,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Better not tell you now.',
+        'probability' => 2,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Very doubtful.',
+        'probability' => 2,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'My reply is no.',
+        'probability' => 2,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'My sources say no.',
+        'probability' => 2,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Without a doubt.',
+        'probability' => 0,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'It is certain.',
+        'probability' => 0,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'As I see it, yes.',
+        'probability' => 0,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Reply hazy, try again.',
+        'probability' => 5,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+    [
+        'text' => 'Cannot predict now.',
+        'probability' => 5,
+        'type' => DyingPhp::TYPE_NO,
+    ],
+
+];
