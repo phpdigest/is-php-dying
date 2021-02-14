c2f6ec93f4eaff1e6c082fb42fba7f75a82170c8
Roman Pronskiy <roman@pronskiy.com>
2021-02-14 13:17:20 +0200

    Remove static stuff and make everything global (•̀ᴗ•́)

--- index.php
+++ index.php
@@ -4,36 +4,20 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-class RandPhpText
+function getRandomLine($texts)
 {
-    private static ?array $texts = null;
+    $probabilityArray = [];
 
-    public static function getRandomLine()
-    {
-        self::ensureTextsLoaded();
-
-        $probabilityArray = [];
-
-        foreach (self::$texts as $key => $tweet) {
-            $lengthFilledProbability = array_fill(0, $tweet['probability'], $key);
-            $probabilityArray = array_merge($probabilityArray, $lengthFilledProbability);
-        }
-
-        $length = count($probabilityArray);
-        $randomKey = random_int(0, $length-1);
-        $tweetKey = $probabilityArray[$randomKey];
-
-
-//        var_dump(self::$texts[$tweetKey]);
-        return self::$texts[$tweetKey]['text'];
+    foreach ($texts as $key => $tweet) {
+        $lengthFilledProbability = array_fill(0, $tweet['probability'], $key);
+        $probabilityArray = array_merge($probabilityArray, $lengthFilledProbability);
     }
 
-    private static function ensureTextsLoaded()
-    {
-        if (null === self::$texts) {
-            self::$texts = include __DIR__ . '/tweets.php';
-        }
-    }
+    $length = count($probabilityArray);
+    $randomKey = random_int(0, $length-1);
+    $tweetKey = $probabilityArray[$randomKey];
+
+    return $texts[$tweetKey]['text'];
 }
 
 $CONSUMER_KEY = getenv('CONSUMER_KEY');
@@ -42,11 +26,12 @@ $ACCESS_TOKEN = getenv('ACCESS_TOKEN');
 $ACCESS_TOKEN_SECRET = getenv('ACCESS_TOKEN_SECRET');
 
 $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
+$tweets = include_once __DIR__ . '/tweets.php';
 
-return function ($event) use ($connection)
+return function ($event) use ($connection, $tweets)
 {
     $result = [];
-    $randomLine = RandPhpText::getRandomLine();
+    $randomLine = getRandomLine($tweets);
     $result[] = $randomLine;
 
     $statues = $connection->post("statuses/update", ["status" => $randomLine]);
